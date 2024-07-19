f2fa8b893b0ed6ac9998349d7c51f3c50f9603e0
juniorbck <correo.junior@gmail.com>
2024-07-18 23:05:12 -0600

    image-picker/#use-the-selected-image

--- 01-StickerSmash/App.js
+++ 01-StickerSmash/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
@@ -9,6 +10,7 @@ import * as ImagePicker from 'expo-image-picker';
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -17,7 +19,7 @@ export default function App() {
     });
 
     if (!result.canceled) {
-      console.log(result);
+      setSelectedImage(result.assets[0].uri);
     } else {
       alert('You did not select any image.');
     }
